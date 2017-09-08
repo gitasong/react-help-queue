09eb2b4f0a8725a80e803f735bd01ab0c75cfa83
Nicole Freed <nicole.freed@gmail.com>
2017-09-08 13:14:24 -0700

    render app

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ var clock = React.createElement('h2', {}, `It is ${new date().toLocaleTimeString
 var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
-  greeting,
+  app,
   document.getElementById('react-app-root')
 );
