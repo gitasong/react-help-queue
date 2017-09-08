61b013c0ac35656b9d82b58755e93175ae3884a2
Nicole Freed <nicole.freed@gmail.com>
2017-09-08 13:07:39 -0700

    create clock h2 element

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 var greeting = React.createElement('h1', {}, 'Hello, World!');
+var clock = React.createElement('h2', {}, `It is ${new date().toLocaleTimeString()}`)
 
 ReactDOM.render(
   greeting,
