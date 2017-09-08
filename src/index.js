da7d3ab7c2c8ba4c3e6c0d2efc7b1ff24b1e7ff3
Nicole Freed <nicole.freed@gmail.com>
2017-09-08 13:11:39 -0700

    create app div element to hold both greeting and clock

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 var greeting = React.createElement('h1', {}, 'Hello, World!');
-var clock = React.createElement('h2', {}, `It is ${new date().toLocaleTimeString()}`)
+var clock = React.createElement('h2', {}, `It is ${new date().toLocaleTimeString()}`);
+var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
   greeting,
