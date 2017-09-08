1bc4b5650e523a24ea3a9abf7f8cf6ddb34edd37
Nicole Freed <nicole.freed@gmail.com>
2017-09-08 14:51:55 -0700

    wrap existing code in liveTime() function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
-var greeting = React.createElement('h1', {}, 'Hello, World!');
-var clock = React.createElement('h2', {}, `It is ${new date().toLocaleTimeString()}`);
+function liveTime() {var greeting = React.createElement('h1', {}, 'Hello, World!');
+var clock = React.createElement('h2', {}, `It is ${new Date().toLocaleTimeString()}`);
 var app = React.createElement('div', {}, greeting, clock);
 
 ReactDOM.render(
   app,
   document.getElementById('react-app-root')
 );
+}
