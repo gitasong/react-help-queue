a5296163f7c6a4edb825a76dc636ef7d5a6918d1
Nicole Freed <nicole.freed@gmail.com>
2017-09-09 09:47:28 -0700

    fix typo

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 ReactDOM.render(
   <div>
     <h1>Help Queue</h1>
-    <h2>{new Date.().toLocaleTimeString()}</h2>
+    <h2>{new Date().toLocaleTimeString()}</h2>
     <h3>3a</h3>
     <p><em>Firebase won't save record</em></p>
-  </div>, 
+  </div>,
   document.getElementById('react-app-root')
 );
