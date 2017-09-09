2060eb5b118f025448240024716b0d674b0cd581
Nicole Freed <nicole.freed@gmail.com>
2017-09-09 15:59:14 -0700

    include react-hot-loader dependencies in webpack-config.js

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ const { resolve } = require('path');
 module.exports = {
 
   entry: [
+    'react-hot-loader/patch',
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
     resolve(__dirname, "src") + "/index.jsx"
   ],
 
