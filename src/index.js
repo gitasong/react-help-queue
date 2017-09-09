6b9047aba803231562f80d755bea1a7f732cfb09
Nicole Freed <nicole.freed@gmail.com>
2017-09-09 09:20:37 -0700

    create ticket elements

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 var heading = React.createElement('h1', {}, 'Help Queue');
-var app = React.createElement('div', {}, heading);
+var ticketLocation = React.createElement('h3', {}, '3a');
+var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
+var ticketIssue = React.createElement('h3', {}, "Firebase won't save record")
+var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
 
 ReactDOM.render(
   app,
