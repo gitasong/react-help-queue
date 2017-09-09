cfe411152b969425a1d6e44c319284209bbec3fe
Nicole Freed <nicole.freed@gmail.com>
2017-09-09 13:31:23 -0700

    move ticket information to Ticket component

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 function Ticket(props) {
-  return ();
+  return (
+    <div>
+      <h3>3A - Thato & Haley</h3>
+      <p><em>Firebase won't save record</em></p>
+      <hr/>
+    </div>
+  );
 }
 
 export default Ticket;
