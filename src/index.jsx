e025b64208245fc7a52b1c0e447438c6856857d8
Nicole Freed <nicole.freed@gmail.com>
2017-09-09 09:26:04 -0700

    refactor in jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,9 @@
-var heading = React.createElement('h1', {}, 'Help Queue');
-var ticketLocation = React.createElement('h3', {}, '3a');
-var ticketNames = React.createElement('h3', {}, 'Thato and Haley');
-var ticketIssue = React.createElement('h3', {}, "Firebase won't save record")
-var app = React.createElement('div', {}, heading, ticketLocation, ticketNames, ticketIssue);
-
 ReactDOM.render(
-  app,
+  <div>
+    <h1>Help Queue</h1>
+    <h2>{new Date.().toLocaleTimeString()}</h2>
+    <h3>3a</h3>
+    <p><em>Firebase won't save record</em></p>
+  </div>, 
   document.getElementById('react-app-root')
 );
