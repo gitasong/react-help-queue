58d27b502700270efa50b890dbf9a4dda154e392
Calla Rudolph and Nicole Freed <nicole@>
2017-09-11 13:31:24 -0700

    create constructor for formVisibleOnPage state

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 class NewTicketControl extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {formVisibleOnPage: false};
+  }
+
   render() {
     return (
       <button>Request Help</button>
