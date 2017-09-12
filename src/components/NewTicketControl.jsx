18ef0ad67a559614cad331d5411d6ed2ac83962d
Brittany Kerr and Nicole Freed <brittany@>
2017-09-12 13:44:02 -0700

    refactor to use bootstrap Buttons

--- src/components/NewTicketControl.jsx
+++ src/components/NewTicketControl.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NewTicketForm from "./NewTicketForm";
 import PropTypes from "prop-types";
+import { Button } from "react-bootstrap";
 
 class NewTicketControl extends React.Component {
 
@@ -26,7 +27,7 @@ class NewTicketControl extends React.Component {
       formAreaContent = <NewTicketForm
         onNewTicketCreation={this.props.onNewTicketCreation}hideFormAfterSubmission = {this.hideForm}/>
     } else {
-      formAreaContent = <button onClick={this.showForm}>Request Help</button>;
+      formAreaContent = <Button onClick={this.showForm}>Request Help</Button>;
     }
 
     return (
